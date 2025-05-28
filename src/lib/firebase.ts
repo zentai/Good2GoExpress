c8dc13a445c680e8e95e87209446bc9324c1ece3
Zen <walao81@gmail.com>
2025-05-28 18:00:13 +0000

    correct cloud storage settings.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAuGujKvKHvFZ1I9GBgT2r81Uqv4AB7SdA", // Replace with your actual API key
   authDomain: "good2go-express.firebaseapp.com",
   projectId: "good2go-express",
-  storageBucket: "good2go-express.appspot.com", // Ensure this is correct (usually project-id.appspot.com)
+  storageBucket: "good2go-express.firebasestorage.app", // Ensure this is correct (usually project-id.appspot.com)
   messagingSenderId: "905482893627",
   appId: "1:905482893627:web:8a9b7ff74b2520160d9f1e"
 };
